6a95d286af7eaf16c46eb9a79a593cc73daf8ee5
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-24 17:23:45 +0000

    add mood add switch case

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  int mood;
+  cout << "Rate your mood on a scale of 1 - 4" << endl;
+  cin >> mood;
+
+  switch(mood) {
+    case 1 : cout << "Play tennis"; break;
+    case 2 : cout << "Play basketball"; break;
+    case 3 :
+    case 4 : cout << "Go for a swim"; break;
+    default: cout << "Go watch a movie";
+  }
+  return 0;
 }
